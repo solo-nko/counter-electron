8c7aa8a5aefcfcd961d54d009e92cf09477d9f48
solo-nko <solowingrazgriz@outlook.com>
2024-08-03 00:36:35 +0900

    Add basic increment/decrement buttons

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,46 @@
 <template>
-<h1>Counter Electron App</h1>
+	<div id="app-container">
+		<h1>Counter Electron App</h1>
+		<h2>{{ count }}</h2>
+		<div class="button" v-on:click="increment(delta)">Increment</div>
+		<div class="button" v-on:click="decrement(delta)" >Decrement</div>
+	</div>
+
 </template>
 
 <script setup lang="ts">
+
+import { ref } from "vue";
+
+const count = ref(0);
+const delta = ref(1);
+
+function increment(increaseBy = 1) {
+	count.value += increaseBy;
+}
+
+function decrement(increaseBy = 1) {
+	count.value -= increaseBy;
+}
+
+function setCount(event:Event) {
+	const target = event.target as HTMLInputElement;
+}
 
 
 </script>
 
 <style scoped>
+
+#app-container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.button {
+	background-color: aliceblue;
+	width: fit-content;
+}
 
 </style>
